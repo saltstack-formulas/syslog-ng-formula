07538cd954bacb6446887f4f58d24865fefa7390
Nicolas Rodriguez <nicoladmin@free.fr>
2017-11-02 21:20:41 +0100

    Add test to trigger Travis build

--- test/integration/syslog_ng/controls/syslog_ng.rb
+++ test/integration/syslog_ng/controls/syslog_ng.rb
@@ -5,3 +5,7 @@ title 'Test SyslogNG installation'
 describe package('syslog-ng') do
   it { should be_installed }
 end
+
+describe file('/etc/syslog-ng/syslog-ng.conf') do
+  it { should exist }
+end
