2b7fab431802a86610d21f2492db8a3e2bff2c21
Nicolas Rodriguez <nicoladmin@free.fr>
2017-11-02 23:06:52 +0100

    Add test on syslog-ng service

--- test/integration/syslog_ng/controls/syslog_ng.rb
+++ test/integration/syslog_ng/controls/syslog_ng.rb
@@ -9,3 +9,9 @@ end
 describe file('/etc/syslog-ng/syslog-ng.conf') do
   it { should exist }
 end
+
+describe sysv_service('syslog-ng') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
