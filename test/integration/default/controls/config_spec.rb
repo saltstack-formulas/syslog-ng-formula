bd2506ff4b0337eeaf4a17659dd394bc158e8c48
Nicolas Rodriguez <nicoladmin@free.fr>
2019-09-25 00:21:09 +0200

    test(inspec): fix test on syslog-ng.conf file

--- test/integration/default/controls/config_spec.rb
+++ test/integration/default/controls/config_spec.rb
@@ -1,18 +1,31 @@
 control 'SyslogNG configuration' do
   title 'should match desired lines'
 
+  str =
+    '''
+source s_src {
+  internal();
+};
+
+destination d_syslog {
+  file("/var/log/syslog");
+};
+
+filter f_syslog3 {
+  not level(debug);
+};
+
+log {
+  source(s_src);
+  filter(f_syslog3);
+  destination(d_syslog);
+};
+'''
+
   describe file('/etc/syslog-ng/syslog-ng.conf') do
-    # # Default config
-    # its('content') { should include '[inputs.diskio]' }
-    # its('content') { should include '[inputs.kernel]' }
-    # its('content') { should include '[inputs.mem]' }
-    # its('content') { should include '[inputs.processes]' }
-    # its('content') { should include '[inputs.swap]' }
-    # its('content') { should include '[inputs.system]' }
-
-    # # Custom config from pillar (default: nothing)
-    # its('content') { should include '[outputs.discard]' }
-
     it { should exist }
+
+    # Custom config from pillar
+    its('content') { should include str }
   end
 end
