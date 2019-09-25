2112711492d13f87c7c39f536d74092c1c13bfc3
Imran Iqbal <iqbalmy@hotmail.com>
2019-09-25 16:00:46 +0100

    feat(platform): add support for `Arch`
    * Semi-automated using https://github.com/myii/ssf-formula/pull/40

--- test/integration/default/controls/service_spec.rb
+++ test/integration/default/controls/service_spec.rb
@@ -1,7 +1,14 @@
+syslogng_service =
+  case platform[:name]
+  when 'arch'
+    'syslog-ng@default.service'
+  else
+    'syslog-ng'
+  end
 control 'SyslogNG service' do
   title 'should be running and enabled'
 
-  describe service('syslog-ng') do
+  describe service(syslogng_service) do
     it { should be_enabled }
     it { should be_running }
   end
